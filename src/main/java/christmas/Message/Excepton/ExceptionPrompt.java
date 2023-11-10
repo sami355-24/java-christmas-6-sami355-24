c5e3ebc58c1efe0fa91444da05e1fb0762451069
sami355-24 <ddung355@gmail.com>
2023-11-11 00:44:35 +0900

    feat(Message/Exception): 예외시 출력한 문구를 관리하는 enum 클래스 작성

---
+++ src/main/java/christmas/Message/Excepton/ExceptionPrompt.java
@@ -0,0 +1,18 @@
+package christmas.Message.Excepton;
+
+import static christmas.Message.Excepton.ExceptionPrefix.*;
+
+public enum ExceptionPrompt {
+    INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+    private String prompt;
+
+    ExceptionPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public String getPrompt() {
+        return ERROR_PREFIX.getPrompt() + prompt;
+    }
+}
