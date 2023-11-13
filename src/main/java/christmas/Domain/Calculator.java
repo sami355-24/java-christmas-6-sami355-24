9abe5db06829d6649e622d21325563f47249ab1e
sami355-24 <ddung355@gmail.com>
2023-11-14 02:58:48 +0900

    refactor(Domain): 불필요한 Calculator의 빈 생성자 삭제

--- src/main/java/christmas/Domain/Calculator.java
+++ src/main/java/christmas/Domain/Calculator.java
@@ -2,8 +2,6 @@ package christmas.Domain;
 
 public class Calculator {
 
-    public Calculator() {
-    }
 
     public int calculateChristmasDiscount(int day) {
         return 1000 + 100 * (day-1);
