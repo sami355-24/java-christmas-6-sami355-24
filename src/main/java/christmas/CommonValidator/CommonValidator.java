830ee9bad588fac5b182bb4b8db2e22dc25fce13
sami355-24 <ddung355@gmail.com>
2023-11-13 11:07:03 +0900

    feat(CommonValidator): 정수 입력을 검증하는 기능이 테스트에 통과하도록 구현

--- src/main/java/christmas/CommonValidator/CommonValidator.java
+++ src/main/java/christmas/CommonValidator/CommonValidator.java
@@ -1,8 +1,17 @@
 package christmas.CommonValidator;
 
+import static christmas.Message.Excepton.ExceptionPrompt.INVALID_INPUT;
+
+import christmas.Exception.CommonValidateException;
+
 public class CommonValidator {
 
     public static void validateIntegerInput(String str) {
+        try {
+            Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new CommonValidateException(INVALID_INPUT.getPrompt(), e);
+        }
     }
 
     public static void validateBlankInput(String str) {
