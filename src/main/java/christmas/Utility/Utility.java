7ffa173b7ef4326cfb4f3bd9e6523a9e8e8a3354
sami355-24 <ddung355@gmail.com>
2023-11-13 11:30:22 +0900

    test(Utility): 문자열을 정수로 변환하는 메서드 작성하고 이에 대한 테스트 코드 추가

---
+++ src/main/java/christmas/Utility/Utility.java
@@ -0,0 +1,11 @@
+package christmas.Utility;
+
+public class Utility {
+
+    private Utility(){
+    }
+
+    public static int convertStringToInt(String str) {
+        return 0;
+    }
+}
---
+++ src/test/java/christmas/Utility/UtilityTest.java
@@ -0,0 +1,17 @@
+package christmas.Utility;
+
+import static christmas.Utility.Utility.convertStringToInt;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UtilityTest {
+
+    @Test
+    @DisplayName("정수로 변환하는 기능을 테스트한다.")
+    void convertStringToIntTest() {
+        assertEquals(1, convertStringToInt("1"));
+    }
+
+}
