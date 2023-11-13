1e7ef828d34a3e452f9224e209030567622387f8
sami355-24 <ddung355@gmail.com>
2023-11-14 03:09:10 +0900

    test(Domain): 할인 적용전 총 금액을 계산하는 로직에 대한 테스트 코드 추가

--- src/test/java/christmas/Domain/CalculatorTest.java
+++ src/test/java/christmas/Domain/CalculatorTest.java
@@ -2,6 +2,7 @@ package christmas.Domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import christmas.Domain.ReservedMenuGroup.ReservedMenuGroup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +15,7 @@ class CalculatorTest {
     private final int discountPriceInWeekday = 4046;
     private final int mainDishCount = 3;
     private final int discountPriceInWeekend = 6069;
-    private final int christmasDay = 25;
+    private final String menuInput = "해산물파스타-2,레드와인-1,초코케이크-1";
 
     @Test
     @DisplayName("크리스마스 디데이 할인 금액을 계산한다.")
@@ -49,5 +50,19 @@ class CalculatorTest {
     void giftBenefitTest(){
         int calculated = calculator.giftBenefit();
         assertEquals(Menu.CHAMPAGNE.getPrice(), calculated);
+    }
+
+    @Test
+    @DisplayName("할인 적용 전 총 금액을 계산한다.")
+    void calculateTotalPriceBeforeDiscountTest(){
+        ReservedMenuGroup menuGroup = new ReservedMenuGroup(menuInput);
+        int calculatedPrice = calculator.calculateTotalPriceBeforeDiscount(menuGroup);
+
+        assertEquals(
+                Menu.SEA_FOOD_PASTA.getPrice() * 2 +
+                        Menu.RED_WINE.getPrice() +
+                        Menu.CHOCO_CAKE.getPrice()
+                , calculatedPrice
+        );
     }
 }
