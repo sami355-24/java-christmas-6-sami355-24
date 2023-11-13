5fe052da570f2e9e8ff34698dae28be1c238a60d
sami355-24 <ddung355@gmail.com>
2023-11-13 10:40:08 +0900

    feat(Exception): 예약 날짜관련 예외클래스 추가

---
+++ src/main/java/christmas/Exception/DateException.java
@@ -0,0 +1,8 @@
+package christmas.Exception;
+
+public class DateException extends IllegalArgumentException{
+
+    public DateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
