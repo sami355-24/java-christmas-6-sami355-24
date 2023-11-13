695f8f80d7cec4a79875484a99d3d9d4b6e98933
sami355-24 <ddung355@gmail.com>
2023-11-14 01:35:42 +0900

    fix(Domain): 음료만 주문 하였는지 검증할때 입력값을 잘못 파싱하는 버그 수정

--- src/main/java/christmas/Domain/ReservedMenuGroup/ReservedMenuGroupValidator.java
+++ src/main/java/christmas/Domain/ReservedMenuGroup/ReservedMenuGroupValidator.java
@@ -18,7 +18,7 @@ public class ReservedMenuGroupValidator {
         List<String> dividedMenus = divideMenusByComma(menuInput);
         for (String menuWithCount : dividedMenus) {
             List<String> dividedMenuAndCount = divideMenuWithCountByHyphen(menuWithCount);
-            if (Menu.isBeverage(Menu.valueOf(dividedMenuAndCount.get(menu)))) {
+            if (Menu.isBeverage(Menu.findMenuByName(dividedMenuAndCount.get(menu)))) {
                 throw new MenuException(INVALID_MENU.getPrompt(), new IllegalArgumentException());
             }
         }
