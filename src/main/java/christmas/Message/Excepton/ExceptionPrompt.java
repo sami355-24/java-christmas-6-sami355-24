9c5456381516614ab7bc9845db5070702ff2cfb5
sami355-24 <ddung355@gmail.com>
2023-11-13 10:48:53 +0900

    feat(Message/Exception): 공통 검증 로직 실패시 출력할 문구 추가

--- src/main/java/christmas/Message/Excepton/ExceptionPrompt.java
+++ src/main/java/christmas/Message/Excepton/ExceptionPrompt.java
@@ -4,7 +4,8 @@ import static christmas.Message.Excepton.ExceptionPrefix.*;
 
 public enum ExceptionPrompt {
     INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    INVALID_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    INVALID_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_INPUT("유효하지 않은 입력입니다. 다시 입력해 주세요.");
 
     private String prompt;
 
