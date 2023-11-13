c4819c2cdbf3c79aaf574d828c015100614d011c
sami355-24 <ddung355@gmail.com>
2023-11-13 23:23:24 +0900

    feat(Domain): 메뉴를 예약하는 기능 추가

--- src/main/java/christmas/Domain/ReservedMenuGroup.java
+++ src/main/java/christmas/Domain/ReservedMenuGroup.java
@@ -15,6 +15,17 @@ public class ReservedMenuGroup {
         this.menuGroup = new EnumMap<>(Menu.class);
     }
 
+    private void reserveMenu(String menus) {
+        List<String> dividedMenus = divideMenusByComma(menus);
+        for (String menuWithCount : dividedMenus) {
+            List<String> dividedMenuAndCount = divideMenuWithCountByHyphen(menuWithCount);
+            menuGroup.put(
+                    Menu.valueOf(dividedMenuAndCount.get(menu)),
+                    Integer.parseInt(dividedMenuAndCount.get(count))
+            );
+        }
+    }
+
     private List<String> divideMenusByComma(String menuInput) {
         return List.of(menuInput.split(COMMA));
     }
