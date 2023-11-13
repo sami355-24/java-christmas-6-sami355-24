73788b3837ff9bd2a11c3d09159e3c2c9acbdece
sami355-24 <ddung355@gmail.com>
2023-11-14 02:41:18 +0900

    feat(Domain): 평일 할인 금액을 계산해주는 로직 추가

--- src/main/java/christmas/Domain/Calculator.java
+++ src/main/java/christmas/Domain/Calculator.java
@@ -1,5 +1,7 @@
 package christmas.Domain;
 
+import christmas.Domain.ReservedMenuGroup.ReservedMenuGroup;
+
 public class Calculator {
 
     public Calculator() {
@@ -7,5 +9,9 @@ public class Calculator {
 
     public int calculateChristmasDiscount(int day) {
         return 1000 + 100 * (day-1);
+    }
+
+    public int calculateWeekdayDiscount(int dessertCount) {
+        return dessertCount * 2023;
     }
 }
