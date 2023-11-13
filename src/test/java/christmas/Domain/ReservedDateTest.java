8821c7470af1af26e3d6b77545bbb0320936f640
sami355-24 <ddung355@gmail.com>
2023-11-13 17:19:18 +0900

    test(Domain): 예약일자에 대한 테스트 코드에서 매직넘버 수정

--- src/test/java/christmas/Domain/ReservedDateTest.java
+++ src/test/java/christmas/Domain/ReservedDateTest.java
@@ -12,24 +12,37 @@ class ReservedDateTest {
     @Test
     @DisplayName("범위에 맞는 날짜로 예약일자 객체를 생성시 정상 수행된다.")
     void createReservedDateTest() {
-        ReservedDate reservedDate = new ReservedDate("1");
+        String validDay = "1";
+        ReservedDate reservedDate = new ReservedDate(validDay);
         assertEquals(1, reservedDate.getDay());
     }
 
     @Test
     @DisplayName("범위에 맞지 않는 날짜로 예약일자 객체를 생성시 예외를 발생시킨다.")
     void createReservedDateExceptionTest1() {
-        assertThrows(DateException.class, () -> new ReservedDate("0"));
-        assertThrows(DateException.class, () -> new ReservedDate("32"));
+        final String invalidRangeDay1 = "0";
+        final String invalidRangeDay2 = "32";
+
+        assertThrows(DateException.class, () -> new ReservedDate(invalidRangeDay1));
+        assertThrows(DateException.class, () -> new ReservedDate(invalidRangeDay2));
     }
 
     @Test
     @DisplayName("숫자이외의 값으로 예약일자 객체를 생성시 예외를 발생시킨다.")
     void createReservedDateExceptionTest2() {
-        assertThrows(CommonValidateException.class, () -> new ReservedDate("a"));
-        assertThrows(CommonValidateException.class, () -> new ReservedDate(""));
-        assertThrows(CommonValidateException.class, () -> new ReservedDate(" "));
+        final String stringInput = "a";
+        final String blankInput = "";
+        final String emptyInput = " ";
+
+        assertThrows(CommonValidateException.class, () -> new ReservedDate(stringInput));
+        assertThrows(CommonValidateException.class, () -> new ReservedDate(blankInput));
+        assertThrows(CommonValidateException.class, () -> new ReservedDate(emptyInput));
     }
 
-
+    @Test
+    @DisplayName("평일 할인에 해당되는 날짜로 예약하였을때 평일 할인에 해당한다고 알려준다.")
+    void isWeekDayTest() {
+        ReservedDate reservedDate = new ReservedDate("1");
+        assertTrue(reservedDate.isWeekDay());
+    }
 }
