28e505b3e46299d83e263084dad7e42d7553787d
sami355-24 <ddung355@gmail.com>
2023-11-13 22:36:57 +0900

    feat(Domain): 주문 가능한 메뉴 추가
    - 에피타이저
    - 메인 요리
    - 음료
    - 디저트

---
+++ src/main/java/christmas/Domain/Menu/Appetizer.java
@@ -0,0 +1,23 @@
+package christmas.Domain.Menu;
+
+public enum Appetizer {
+    MUSHROOM_SOUP("양송이스프", 6_000),
+    TAPAS("타파스", 5_500),
+    CAESAR_SALAD("시저샐러드", 8_000);
+
+    private final String name;
+    private final int price;
+
+    Appetizer(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
