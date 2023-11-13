14dbd6e4b9f12e13a5d3360969fab2c8685f1b28
sami355-24 <ddung355@gmail.com>
2023-11-14 02:44:40 +0900

    test(Domain): 주중 할인 금액을 계산하는 로직을 테스트 하는 코드 추가

--- src/test/java/christmas/Domain/CalculatorTest.java
+++ src/test/java/christmas/Domain/CalculatorTest.java
@@ -9,14 +9,22 @@ class CalculatorTest {
 
     private final Calculator calculator = new Calculator();
     private final int normalDay = 4;
+    private final int discountPriceInNormalDay = 1300;
+    private final int dessertCount = 2;
+    private final int discountPriceInWeekday = 4046;
     private final int christmasDay = 25;
-    private final int weekendDay = 1;
-    private final int starDay = 3;
 
     @Test
     @DisplayName("크리스마스 디데이 할인 금액을 계산한다.")
-    void calculateChristmasDiscount() {
+    void calculateChristmasDiscountTest() {
         int calculated = calculator.calculateChristmasDiscount(normalDay);
-        assertEquals(1300, calculated);
+        assertEquals(discountPriceInNormalDay, calculated);
+    }
+
+    @Test
+    @DisplayName("주중 할인 금액을 계산한다.")
+    void calculateWeekdayDiscountTest(){
+        int calculated = calculator.calculateWeekdayDiscount(dessertCount);
+        assertEquals(discountPriceInWeekday, calculated);
     }
 }
