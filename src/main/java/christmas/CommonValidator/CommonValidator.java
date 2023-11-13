029069ce9ee3051add0badcf6a8ae3fcfe79ba4f
sami355-24 <ddung355@gmail.com>
2023-11-13 10:56:39 +0900

    feat(CoomonValidator): 공통 검증 메소드 구조 추가

---
+++ src/main/java/christmas/CommonValidator/CommonValidator.java
@@ -0,0 +1,13 @@
+package christmas.CommonValidator;
+
+public class CommonValidator {
+
+    public static void validateIntegerInput(String str) {
+    }
+
+    public static void validateBlankInput(String str) {
+    }
+
+    public static void validateEmptyInput(String str) {
+    }
+}
