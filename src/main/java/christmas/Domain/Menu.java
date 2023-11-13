f60dae5324473f2eb950ac3ebb61d8c91f023c1d
sami355-24 <ddung355@gmail.com>
2023-11-13 22:50:08 +0900

    refactor(Domain): 에피타이저, 메인, 디저트, 음료를 나누었던 메뉴 enum클래스를 하나의 enum 클래스로 관리하도록 변경

---
+++ src/main/java/christmas/Domain/Menu.java
@@ -0,0 +1,41 @@
+package christmas.Domain;
+
+public enum Menu {
+    MUSHROOM_SOUP("애피타이저" ,"양송이스프", 6_000),
+    TAPAS("애피타이저", "타파스", 5_500),
+    CAESAR_SALAD("애피타이저", "시저샐러드", 8_000),
+
+    T_BONE_STEAK("메인", "티본스테이크", 55_000),
+    BARBECUE_RIB("메인", "바베큐립", 54_000),
+    SEAFOOD_PASTA("메인", "해산물파스타", 35_000),
+    CHRISTMAS_PASTA("메인", "크리스마스파스타", 25_000),
+
+    CHOCO_CAKE("디저트", "초코케이크", 15_000),
+    ICE_CREAM("디저트", "아이스크림", 5_000),
+
+    ZERO_COKE("음료", "제로콜라", 3_000),
+    RED_WINE("음료", "레드와인", 60_000),
+    CHAMPAGNE("음료", "샴페인", 25_000);
+
+    private final String category;
+    private final String name;
+    private final int price;
+
+    Menu(String category, String name, int price) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
