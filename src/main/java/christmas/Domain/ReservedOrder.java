35f0b58bbc674040cd8f793e551a606592275195
sami355-24 <ddung355@gmail.com>
2023-11-14 15:20:56 +0900

    docs(docs): 혜택 내역을 의미하는 클래스의 기능 목록 추가

--- src/main/java/christmas/Domain/ReservedOrder.java
+++ src/main/java/christmas/Domain/ReservedOrder.java
@@ -1,5 +1,18 @@
 package christmas.Domain;
 
+import christmas.Domain.ReservedMenuGroup.ReservedMenuGroup;
+
 public class ReservedOrder {
+
+    private final ReservedMenuGroup menuGroup;
+    private final ReservedDate date;
+    private final Calculator calculator;
+
+    public ReservedOrder(ReservedMenuGroup menuGroup, ReservedDate date, Calculator calculator) {
+        this.menuGroup = menuGroup;
+        this.date = date;
+        this.calculator = calculator;
+    }
+
 
 }
