263e22e045351114e95fc7a6b10956b1a9247ef8
sami355-24 <ddung355@gmail.com>
2023-11-14 16:54:41 +0900

    feat(View): 프로그램 시작시 안내문구 출력하는 로직 구현

---
+++ src/main/java/christmas/View/OutputVIew.java
@@ -0,0 +1,15 @@
+package christmas.View;
+
+import static christmas.Message.EventPlannerPrompt.WELL_COME;
+
+public class OutputVIew {
+
+    public OutputVIew() {
+    }
+
+    public void welcomeMessage(){
+        System.out.println(WELL_COME.getPrompt());
+    }
+
+
+}
