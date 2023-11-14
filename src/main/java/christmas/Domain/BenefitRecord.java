fcc8702d9f3aebefbbbb802f3d23679dbc37f209
sami355-24 <ddung355@gmail.com>
2023-11-14 15:14:53 +0900

    fix(Domain): 혜택 내역을 생성하는 로직 수정

--- src/main/java/christmas/Domain/BenefitRecord.java
+++ src/main/java/christmas/Domain/BenefitRecord.java
@@ -8,14 +8,33 @@ import java.util.Map;
 
 public class BenefitRecord {
 
+    private final String CHRISTMAS_BENEFIT = "크리스마스 디데이 할인";
+    private final String WEEKDAY_BENEFIT = "평일 할인";
+    private final String WEEKEND_BENEFIT = "주말 할인";
+    private final String SPECIAL_BENEFIT = "특별 할인";
+    private final String GIFT_BENEFIT = "증정 이벤트";
+    private final int GIFT_BENEFIT_CRITERIA = 120_000;
+
     Map<String, Integer> benefits;
+    Calculator calculator = new Calculator();
 
     BenefitRecord(ReservedMenuGroup menuGroup, ReservedDate date) {
         this.benefits = new HashMap<>();
+
+        if (date.isChristmasNotPassed())
+            addBenefit(CHRISTMAS_BENEFIT, calculator.calculateChristmasDiscount(date.getDay()));
+        if (date.isWeekDay())
+            addBenefit(WEEKDAY_BENEFIT, calculator.calculateWeekdayDiscount(menuGroup.countDessert()));
+        if (date.isWeekend())
+            addBenefit(WEEKEND_BENEFIT, calculator.calculateWeekendDiscount(menuGroup.countMainDish()));
+        if (date.isStarDay())
+            addBenefit(SPECIAL_BENEFIT, calculator.specialDiscount());
+        if (calculator.calculateTotalPriceBeforeDiscount(menuGroup) >= GIFT_BENEFIT_CRITERIA)
+            addBenefit(GIFT_BENEFIT, calculator.giftBenefit());
     }
 
-    public void addBenefit(String benefitName, int benefitAmount) {
-        benefits.put(benefitName, benefitAmount);
+    private void addBenefit(String benefitName, int benefitAmount) {
+        benefits.put(benefitName, -benefitAmount);
     }
 
     public Map<String, Integer> getBenefits() {
