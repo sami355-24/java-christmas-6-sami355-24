c29dfb2538942f99a50042db2e4eaa21ab00532a
sami355-24 <ddung355@gmail.com>
2023-11-14 20:47:17 +0900

    feat: 일반 컨트롤러로 시작점을 잡는것이 아닌 데코레이터를 적용한 컨트롤러로 시작잠 잡게 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,10 @@
 package christmas;
 
-import christmas.Controller.EventPlanner;
+import christmas.Controller.EventPlannerDecorator;
 
 public class Application {
     public static void main(String[] args) {
-        EventPlanner eventPlanner = new EventPlanner();
+        EventPlannerDecorator eventPlanner = new EventPlannerDecorator();
         eventPlanner.start();
     }
 }
