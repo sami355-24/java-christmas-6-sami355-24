ed5d80fe32312e5eadfe0b2dc5af2c1b085d57fd
sami355-24 <ddung355@gmail.com>
2023-11-14 17:03:52 +0900

    feat(View): 주문 내역을 출력하는 로직 구현

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -1,7 +1,14 @@
 package christmas.View;
 
 import static christmas.Message.EventPlannerPrompt.PREVIEW_EVENT_BENEFITS;
+import static christmas.Message.EventPlannerPrompt.RESERVED_MENU;
+import static christmas.Message.EventPlannerPrompt.RESERVED_MENU_TITLE;
 import static christmas.Message.EventPlannerPrompt.WELL_COME;
+
+import christmas.Domain.Menu;
+import christmas.Domain.ReservedMenuGroup.ReservedMenuGroup;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class OutputView {
 
@@ -14,5 +21,15 @@ public class OutputView {
 
     public void showPreviewReservedOrderMessage(int day){
         System.out.println(String.format(PREVIEW_EVENT_BENEFITS.getPrompt(), day));
+    }
+
+    public void showReservedMenu(ReservedMenuGroup menuGroup){
+        System.out.println(RESERVED_MENU_TITLE.getPrompt());
+
+        for (Entry<Menu, Integer> menu : menuGroup.getMenuGroup().entrySet()) {
+            String menuName = menu.getKey().getName();
+            Integer menuCount = menu.getValue();
+            System.out.println(String.format(RESERVED_MENU.getPrompt(), menuName, menuCount));
+        }
     }
 }
