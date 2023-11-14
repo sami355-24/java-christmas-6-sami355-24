24ae701b049599d0e511391ff8f7ed1e1755cadb
sami355-24 <ddung355@gmail.com>
2023-11-15 02:00:17 +0900

    test(Domain): 적용된 이벤트 목록에 대한 테스트를 진행하는 클래스에서 불필요한 변수 삭제

--- src/test/java/christmas/Domain/AppliedEventGroupTest.java
+++ src/test/java/christmas/Domain/AppliedEventGroupTest.java
@@ -2,50 +2,45 @@ package christmas.Domain;
 
 import static christmas.Constant.Event.SANTA_BADGE;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import christmas.Domain.ReservedMenuGroup.ReservedMenuGroup;
 import java.util.List;
 import java.util.Map;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AppliedEventGroupTest {
 
-    private final String validMenuInput1 = "해산물파스타-2,레드와인-1,초코케이크-1";
-    private final String validMenuInput2 = "티본스테이크-2";
-    private final ReservedMenuGroup reservedMenuGroup1 = new ReservedMenuGroup(validMenuInput1);
-    private final ReservedMenuGroup reservedMenuGroup2 = new ReservedMenuGroup(validMenuInput2);
+    private final String validMenuInput = "해산물파스타-2,레드와인-1,초코케이크-1";
+    private final ReservedMenuGroup reservedMenuGroup = new ReservedMenuGroup(validMenuInput);
     private final ReservedDate date = new ReservedDate("3");
-    private AppliedEventGroup appliedEventGroup1 = new AppliedEventGroup(reservedMenuGroup1, date);
-    private AppliedEventGroup appliedEventGroup2 = new AppliedEventGroup(reservedMenuGroup2, date);
+    private AppliedEventGroup appliedEventGroup = new AppliedEventGroup(reservedMenuGroup, date);
 
     @Test
     @DisplayName("올바른 배지를 받을 수 있는지 확인한다.")
     void getBadge() {
-        String badge = appliedEventGroup1.getBadge();
+        String badge = appliedEventGroup.getBadge();
         assertThat(badge).isEqualTo(SANTA_BADGE.getValue());
     }
 
     @Test
     @DisplayName("이벤트 금액의 총합을 구한다.")
     void findTotalEventAmount() {
-        int totalEventAmount = appliedEventGroup1.findTotalEventAmount();
+        int totalEventAmount = appliedEventGroup.findTotalEventAmount();
         assertThat(totalEventAmount).isEqualTo(29_223);
     }
 
     @Test
     @DisplayName("이벤트 금액들이 있는 리스트를 구한다.")
     void findEventAmounts() {
-        List<Integer> eventAmounts1 = appliedEventGroup1.findEventAmounts();
+        List<Integer> eventAmounts1 = appliedEventGroup.findEventAmounts();
         assertThat(eventAmounts1).containsExactly(25_000, 1_200,2_023,1_000);
     }
 
     @Test
     @DisplayName("적용된 이벤트가 있는 리스트를 구한다.")
     void getAppliedEventGroup() {
-        Map<String, Integer> appliedEventGroup = appliedEventGroup1.getAppliedEventGroup();
+        Map<String, Integer> appliedEventGroup = this.appliedEventGroup.getAppliedEventGroup();
         assertThat(appliedEventGroup.size()).isEqualTo(4);
     }
 }
